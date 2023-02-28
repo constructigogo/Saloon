61fa66f08d20c3e26a359a22365df29375b05003
MAGNIN CONSTANTIN p1806593 <constantin.magnin@etu.univ-lyon1.fr>
2023-02-28 15:44:52 +0100

    entities icons bound to camera view

--- src/space/project.rs
+++ src/space/project.rs
@@ -9,21 +9,23 @@ use crate::space::galaxy::Rendered;
 
 pub fn project_to_camera(camera_zoom: Res<CameraZoom>,
                          camera_id: Res<CameraID>,
-                         mut camera_query: Query<&Camera>,
-                         mut query: Query<(&mut Transform, &SimPosition), (With<Rendered>, Without<SolarSystem>)>) {
+                         camera_query: Query<(&Camera, &Transform)>,
+                         mut query: Query<(&mut Transform, &SimPosition), (With<Rendered>, Without<SolarSystem>, Without<Camera>)>) {
 
     //println!("projecting {:?} objects ", query.iter().len());
-    let got = camera_query.get_mut(camera_id.0);
-    match got{
+    let got: Result<(&Camera, &Transform), QueryEntityError> = camera_query.get(camera_id.0);
+    match got {
         Ok(cam) => {
+            let camera: &Camera = cam.0;
+            let transf: &Transform = cam.1;
             for (mut trans, sPos) in query.iter_mut() {
                 let calc = Vec3 {
                     x: ((sPos.0.x / camera_zoom.0) as f32).clamp(
-                        -(((cam.physical_target_size().unwrap().x -4)/2) as f32),
-                        ((cam.physical_target_size().unwrap().x -4)/2) as f32),
+                        (transf.translation.x -((camera.physical_viewport_size().unwrap().x - 48) / 2) as f32),
+                        (transf.translation.x +((camera.physical_viewport_size().unwrap().x - 48) / 2) as f32)),
                     y: ((sPos.0.y / camera_zoom.0) as f32).clamp(
-                        -(((cam.physical_target_size().unwrap().y -4)/2) as f32),
-                        ((cam.physical_target_size().unwrap().y -4)/2) as f32),
+                        (transf.translation.y -((camera.physical_viewport_size().unwrap().y - 48) / 2) as f32),
+                        (transf.translation.y +((camera.physical_viewport_size().unwrap().y - 48) / 2) as f32)),
                     z: 0.0,
                 };
                 trans.translation = calc
@@ -31,5 +33,4 @@ pub fn project_to_camera(camera_zoom: Res<CameraZoom>,
         }
         Err(_) => {}
     }
-
 }
